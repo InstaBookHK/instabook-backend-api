d46628e50036bb4b2149dceed0a6906035717557
Tom Tang <jjjj1997@live.hk>
2023-08-07 00:16:04 +0800

    feat: exchange-code auto get user / create user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 // src/auth/auth.controller.ts
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
+import { ExchangeCodeResponse } from './dto/exchange-code.dto';
 import { LoginDto } from './dto/login.dto';
 import { NewPasswordDto } from './dto/new-password.dto';
 import { CodeDto, ConfirmSignUpDto } from './dto/otp.dto';
@@ -12,15 +14,34 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
+  // TODO properly rename this endpoint
+  // * add swagegr api response type
+  // * add response type
+  // * add response type
+  @ApiResponse({
+    status: 200,
+    description: 'exchange code for token and get user / create user',
+    type: ExchangeCodeResponse,
+  })
   @Post('exchange-code')
-  async exchangeCode(@Body('code') code: string) {
+  async exchangeCode(
+    @Body('code') code: string,
+  ): Promise<ExchangeCodeResponse> {
     const tokens = await this.authService.exchangeCodeForToken(code);
-    // Here you can pass tokens to AuthService for any additional processing
-    // For example:
-    // await this.authService.handleAuthentication(tokens);
-    return tokens;
+    // 1. using id_token to query user exist in db or not
+    // 2. if user not exist, create user in db
+    // 3. if user exist, return user with tokens as response
+    const user = await this.userService.getUserByCognitoId(tokens.id_token);
+    if (!user) {
+      const newUser = await this.userService.create(tokens.id_token);
+      return { user: newUser, tokens };
+    }
+    return { user, tokens };
   }
 
   @Post('login')
